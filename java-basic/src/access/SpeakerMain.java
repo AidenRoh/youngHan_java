2446f85b973514be76b173bf7fd100f716d988c1
AidenRoh <colohrby@gmail.com>
2023-12-19 23:05:52 +0900

    feat: section 6, package

--- java-basic/src/access/SpeakerMain.java
+++ java-basic/src/access/SpeakerMain.java
@@ -11,7 +11,7 @@ public class SpeakerMain {
 		speaker.volumeUp();
 		speaker.showVolume();
 
-		//필드에 직접 접근
+		//필드에 직접 접근하여 정보값을 바꾸는 것은 위험하다!!
 		System.out.println("volume 필드에 직접 접근");
 		//speaker.volume = 200;
 		speaker.showVolume();
