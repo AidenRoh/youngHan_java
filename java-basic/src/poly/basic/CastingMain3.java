3c3a7038056d373c36384e883ff22d573e899d98
AidenRoh <colohrby@gmail.com>
2024-01-11 23:26:21 +0900

    chore: section 10, polymorphism 2

---
+++ java-basic/src/poly/basic/CastingMain3.java
@@ -0,0 +1,15 @@
+package poly.basic;
+
+//upcasting vs downcasting
+public class CastingMain3 {
+
+	public static void main(String[] args) {
+
+		Child child = new Child();
+		Parent parent1 = (Parent) child; // 업캐스팅은 생략 가능, 생략 권장
+		Parent parent2 = child; //업캐스팅 생략
+
+		parent1.parentMethod();
+		parent2.parentMethod();
+	}
+}
