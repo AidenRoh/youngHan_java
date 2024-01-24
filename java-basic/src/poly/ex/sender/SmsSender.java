e063041592afba543891498f2714d9ef66b085ad
AidenRoh <colohrby@gmail.com>
2024-01-24 18:46:20 +0900

    feature: add polymorphism

---
+++ java-basic/src/poly/ex/sender/SmsSender.java
@@ -0,0 +1,8 @@
+package poly.ex.sender;
+
+public class SmsSender implements Sender {
+	@Override
+	public void sendMessage(String message) {
+		System.out.println("SMS를  발송합니다: " + message);
+	}
+}
